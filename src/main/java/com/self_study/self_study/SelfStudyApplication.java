87c4c66a391d934d197f7338d3fcf44d6a3bf9b2
duongbui <duongbuibd@gmail.com>
2025-03-16 21:38:22 +0700

    feat: crud user

---
+++ src/main/java/com/self_study/self_study/SelfStudyApplication.java
@@ -0,0 +1,13 @@
+package com.self_study.self_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/self_study/self_study/SelfStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self_study.self_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
